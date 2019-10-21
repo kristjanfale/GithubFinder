f09c32170c40234e8137bcc601471971a00f3728
Kristjan Fale <kristjan.fale@gmail.com>
2019-10-21 19:26:29 +0200

    fix

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div className='text-center'>
       <h1>About This App</h1>
       <p>Search Github users and their Repos</p>
-      <p>Build with React - Hooks &amp; Context API</p>
+      <p>Built with React - Hooks &amp; Context API</p>
       <p>Version: 2.0.0</p>
     </div>
   );
